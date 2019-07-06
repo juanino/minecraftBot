2ea7770b2c4e72f695d5e30ab7f1ed8062b728c2
jerry <juanino@gmail.com>
2019-07-06 13:47:42 +0100

    sample config file

---
+++ botconfig_sample.py
@@ -0,0 +1,8 @@
+# file is read in as "cfg" in main prog
+# see https://martin-thoma.com/configuration-files-in-python/ for more examples
+
+token = 'SECRET TOKEN'
+webhook_safe = 'https://discordapp.com/api/webhooks/XXXX/XXXXXXXX'
+bot_channel_id = 'bot channel id #'
+instance_id = 'the ec2 instance id you want to control i-XXXXXXX'
+master_channel = 'minecraft-messages' # the channel to listen on for commands, matches a channel name on your server
